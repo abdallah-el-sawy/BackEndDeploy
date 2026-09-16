07f8e207d490361184d242d104e45192d50a3f86
Abdallah Elsawy <abdallahelsawy706@gmail.com>
2026-09-17 02:41:31 +0300

    Update server.js

--- CRASH_NODE/server.js
+++ CRASH_NODE/server.js
@@ -22,6 +22,6 @@ mongoose
 
 app.use(articleRouter);
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
