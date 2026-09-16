b3ccf4700805ae9fc17f1cbbbae310d0997ec467
Abdallah Elsawy <abdallahelsawy706@gmail.com>
2026-09-17 02:52:54 +0300

    Integrate MongoDB connection in server.js
    Added MongoDB connection setup with error handling.

--- CRASH_NODE/server.js
+++ CRASH_NODE/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const articleRouter = require("./Routes/articleRoute");
@@ -10,6 +11,15 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Backend is working!");
 });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch((error) => {
+    console.log("MongoDB Error:", error.message);
+  });
 
 app.use(articleRouter);
 
