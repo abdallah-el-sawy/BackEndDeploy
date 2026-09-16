dc159d6db642178b3465b6cad9ecab556dfd8207
Abdallah Elsawy <abdallahelsawy706@gmail.com>
2026-09-17 02:48:04 +0300

    Remove MongoDB connection from server.js
    Removed MongoDB connection logic from server.js.

--- CRASH_NODE/server.js
+++ CRASH_NODE/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mongoose = require("mongoose");
 
 const app = express();
 const articleRouter = require("./Routes/articleRoute");
@@ -11,15 +10,6 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Backend is working!");
 });
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected successfully");
-  })
-  .catch((error) => {
-    console.log("MongoDB Error:", error.message);
-  });
 
 app.use(articleRouter);
 
